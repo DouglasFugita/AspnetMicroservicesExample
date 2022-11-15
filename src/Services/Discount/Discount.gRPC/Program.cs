2c06fdb224bc360e5f4649d0c8ed5a5e84cf0ca6
Douglas Fugita <douglas.cezar@gmail.com>
2022-11-14 21:40:56 -0300

    Discount.gRPC - Entities e Repositories

--- src/Services/Discount/Discount.gRPC/Program.cs
+++ src/Services/Discount/Discount.gRPC/Program.cs
@@ -1,4 +1,6 @@
+using Discount.gRPC.Repositories;
 using Discount.gRPC.Services;
+using Npgsql;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,8 +9,51 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddGrpc();
+builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var configuration = services.GetRequiredService<IConfiguration>();
+
+    try
+    {
+
+        using var connection = new NpgsqlConnection(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+        connection.Open();
+
+        using var command = new NpgsqlCommand
+        {
+            Connection = connection
+        };
+
+        command.CommandText = "DROP TABLE IF EXISTS Coupon";
+        command.ExecuteNonQuery();
+
+        command.CommandText = @"CREATE TABLE Coupon(Id SERIAL PRIMARY KEY, 
+                                                                ProductName VARCHAR(24) NOT NULL,
+                                                                Description TEXT,
+                                                                Amount INT)";
+        command.ExecuteNonQuery();
+
+        command.CommandText = "INSERT INTO Coupon(ProductName, Description, Amount) VALUES('IPhone X', 'IPhone Discount', 150);";
+        command.ExecuteNonQuery();
+
+        command.CommandText = "INSERT INTO Coupon(ProductName, Description, Amount) VALUES('Samsung 10', 'Samsung Discount', 100);";
+        command.ExecuteNonQuery();
+
+        command.CommandText = "INSERT INTO Coupon(ProductName, Description, Amount) VALUES('Samsung 20', 'Samsung Discount', 100);";
+        command.ExecuteNonQuery();
+
+    }
+    catch (Exception)
+    {
+
+    }
+
+}
 
 // Configure the HTTP request pipeline.
 app.MapGrpcService<GreeterService>();
