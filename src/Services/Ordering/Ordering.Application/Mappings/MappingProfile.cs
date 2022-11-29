d972543b25a42cc02177d3f2af0dfda9ae8ce2e6
Douglas Fugita <douglas.cezar@gmail.com>
2022-11-29 12:24:59 -0300

    Ordering.Application - UpdateOrder Command

--- src/Services/Ordering/Ordering.Application/Mappings/MappingProfile.cs
+++ src/Services/Ordering/Ordering.Application/Mappings/MappingProfile.cs
@@ -1,12 +1,8 @@
 ﻿using AutoMapper;
 using Ordering.Application.Features.Orders.Commands.CheckoutOrder;
+using Ordering.Application.Features.Orders.Commands.UpdateOrder;
 using Ordering.Application.Features.Orders.Queries.GetOrdersList;
 using Ordering.Domain.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Ordering.Application.Mappings;
 public class MappingProfile : Profile
@@ -15,5 +11,6 @@ public class MappingProfile : Profile
     {
         CreateMap<Order, OrdersVm>().ReverseMap();
         CreateMap<Order, CheckoutOrderCommand>().ReverseMap();
+        CreateMap<Order, UpdateOrderCommand>().ReverseMap();
     }
 }
