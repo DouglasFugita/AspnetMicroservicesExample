2cf1a5ff24b47466f75ba0c698b3565aa0d012f2
Douglas Fugita <douglas.cezar@gmail.com>
2022-11-29 14:15:19 -0300

    Ordering.Application - DeleteOrder Command

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/DeleteOrder/DeleteOrderCommandHandler.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Ordering.Application.Contracts.Infrastructure;
+using Ordering.Application.Contracts.Persistence;
+using Ordering.Application.Features.Orders.Commands.CheckoutOrder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Features.Orders.Commands.DeleteOrder;
+public class DeleteOrderCommandHandler : IRequestHandler<DeleteOrderCommand>
+{
+    private readonly IOrderRepository _orderRepository;
+    private readonly IMapper _mapper;
+    private readonly ILogger<CheckoutOrderCommandHandler> _logger;
+
+    public DeleteOrderCommandHandler(IOrderRepository orderRepository, IMapper mapper, ILogger<CheckoutOrderCommandHandler> logger)
+    {
+        _orderRepository = orderRepository;
+        _mapper = mapper;
+        _logger = logger;
+    }
+
+    public async Task<Unit> Handle(DeleteOrderCommand request, CancellationToken cancellationToken)
+    {
+        var orderToDelete = await _orderRepository.GetByIdAsync(request.Id);
+        if (orderToDelete == null)
+        {
+            _logger.LogError("Order no exist on database");
+        }
+        await _orderRepository.DeleteAsync(orderToDelete);
+        _logger.LogError($"Order {orderToDelete.Id} was deleted.");
+
+        return Unit.Value;
+    }
+}
