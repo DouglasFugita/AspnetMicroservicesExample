e2db8335143d7dfa83abc08a5f8f608e0436bc90
Douglas Fugita <douglas.cezar@gmail.com>
2022-11-30 18:23:20 -0300

    Ordering.Infra - Persistence

---
+++ src/Services/Ordering/Ordering.Infra/Persistence/OrderContextSeed.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Logging;
+using Ordering.Domain.Entities;
+
+namespace Ordering.Infra.Persistence;
+public class OrderContextSeed
+{
+    public static async Task SeedAsync(OrderContext orderContext, ILogger<OrderContextSeed> logger)
+    {
+        if (!orderContext.Orders.Any())
+        {
+            orderContext.Orders.AddRange(GetPreconfiguredOrders());
+            await orderContext.SaveChangesAsync();
+            logger.LogInformation("Seed database associated with context {DbContextName}", typeof(OrderContext).Name);
+        }
+    }
+
+    private static IEnumerable<Order> GetPreconfiguredOrders()
+    {
+        return new List<Order>
+        {
+            new Order()
+            {
+                UserName = "usuario",
+                FirstName = "Doug",
+                LastName = "Fugita", 
+                EmailAddress = "usuario@email.com", 
+                AddressLine = "Endereco", 
+                Country = "Brazil", 
+                TotalPrice = 350
+            }
+        };
+    }
+}
