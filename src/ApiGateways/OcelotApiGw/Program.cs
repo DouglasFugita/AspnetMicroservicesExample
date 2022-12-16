e371b010776e6174814c697c52bfcb2f7dfbf2ea
Douglas Fugita <douglas.cezar@gmail.com>
2022-12-16 12:11:19 -0300

    OcelotApiGw - Setup do Ocelot e criacao do ocelot.json

--- src/ApiGateways/OcelotApiGw/Program.cs
+++ src/ApiGateways/OcelotApiGw/Program.cs
@@ -1,8 +1,18 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Logging.AddConfiguration(builder.Configuration.GetSection("Logging"));
+builder.Logging.AddConsole();
+builder.Logging.AddDebug();
+
+IConfiguration configuration = new ConfigurationBuilder().AddJsonFile($"ocelot.{builder.Environment.EnvironmentName}.json",true,true).Build();
+builder.Services.AddOcelot(configuration);
+
 
 var app = builder.Build();
 
+await app.UseOcelot();
 
 app.MapGet("/", () => "Hello World!");
 
