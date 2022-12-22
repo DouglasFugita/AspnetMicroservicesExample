3efe8a703e965bb95fb72a194db76ee8a0741e9d
Douglas Fugita <douglas.cezar@gmail.com>
2022-12-22 18:53:14 -0300

    Docker - Alteracao nas dependencias nos Docker files.

--- src/ApiGateways/OcelotApiGw/Program.cs
+++ src/ApiGateways/OcelotApiGw/Program.cs
@@ -1,11 +1,10 @@
+using Common.Logging;
 using Ocelot.Cache.CacheManager;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Logging.AddConfiguration(builder.Configuration.GetSection("Logging"));
-builder.Logging.AddConsole();
-builder.Logging.AddDebug();
 
 //IConfiguration configuration = new ConfigurationBuilder().AddJsonFile($"ocelot.{builder.Environment.EnvironmentName}.json",true,true).Build();
 IConfiguration configuration = new ConfigurationBuilder().AddJsonFile($"ocelot.json").Build();
@@ -16,7 +15,7 @@ builder.Services.AddOcelot(configuration)
     })
     ;
 
-
+builder.Host.UseSerilog(SeriLogger.Configure);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
